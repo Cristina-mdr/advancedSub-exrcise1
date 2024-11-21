69b79bcbb4729ae15869a7e031fc537e2dad236a
cris <you@example.com>
2024-11-21 23:41:29 +0200

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+import { connect } from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
